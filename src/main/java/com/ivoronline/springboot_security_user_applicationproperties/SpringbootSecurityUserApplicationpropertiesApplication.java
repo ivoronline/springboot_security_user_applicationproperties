99e703b089ee8ce4df84ec79047ca7e7a92ba24a
igoreta <ivoronline@gmail.com>
2021-02-01 14:57:56 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_security_user_applicationproperties/SpringbootSecurityUserApplicationpropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_security_user_applicationproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityUserApplicationpropertiesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSecurityUserApplicationpropertiesApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_security_user_applicationproperties/SpringbootSecurityUserApplicationpropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_security_user_applicationproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityUserApplicationpropertiesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
