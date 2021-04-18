fbebf9ad2b253d0e7c41d4ec4583db19f7e356fb
igoreta <ivoronline@gmail.com>
2021-04-18 10:04:39 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_security_user_applicationproperties/SpringbootSecurityUserApplicationpropertiesApplication.java
+++ src/main/java/com/ivoronline/springboot_security_user_applicationproperties/SpringbootSecurityUserApplicationpropertiesApplication.java
@@ -2,8 +2,10 @@ package com.ivoronline.springboot_security_user_applicationproperties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringbootSecurityUserApplicationpropertiesApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/ivoronline/springboot_security_user_applicationproperties/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_user_applicationproperties/controllers/MyController.java
@@ -1,11 +1,13 @@
 package com.ivoronline.springboot_security_user_applicationproperties.controllers;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
+  @Secured("ROLE_ADMIN")
   @RequestMapping("/Hello")
   public String hello() {
     return "Hello from Controller";
